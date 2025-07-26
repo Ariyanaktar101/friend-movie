ea01a0bdd9be581c9c7378ac391efeaf13206865
Ariyanaktar101 <ariyan.official101@gmail.com>
2025-07-26 14:10:30 +0530

    yes

--- movies-app-FullStack--main/Frontend/API/apiconfig.js
+++
@@ -1,1 +0,0 @@
-export const API_URL = 'http://192.168.100.21:5000/api/auth';
--- movies-app-FullStack--main/Frontend/index.js
+++
@@ -1,4 +0,0 @@
-import { registerRootComponent } from 'expo';
-import App from './App'; // <-- use relative path './App' if in same folder
-
-registerRootComponent(App);
